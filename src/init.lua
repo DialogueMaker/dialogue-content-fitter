204e944ee3b556297afb74be7eb77d61c8bff765
Christian Toney <christian.toney@beastslash.com>
2025-06-02 15:13:52 -0400

    seperate functions and add docs

--- src/init.lua
+++ src/init.lua
@@ -103,40 +103,194 @@ function DialogueContentFitter:clonePages(pages: {Page}): {Page}
 
 end;
 
+function DialogueContentFitter:getRichTextTags(text: string): {RichTextTag}
+
+  local richTextTagIndices: {RichTextTag} = {};
+  local openTagIndices: {number} = {};
+  local textCopy = text;
+  local tagPattern = "<[^<>]->";
+  local pointer = 1;
+  for tag in textCopy:gmatch(tagPattern) do
+
+    -- Get the tag name and attributes.
+    local tagText = tag:match("<([^<>]-)>");
+    if tagText then
+
+      local firstSpaceIndex = tagText:find(" ");
+      local tagTextLength = tagText:len();
+      local name = tagText:sub(1, (firstSpaceIndex and firstSpaceIndex - 1) or tagTextLength);
+      if name:sub(1, 1) == "/" then
+
+        for _, index in openTagIndices do
+
+          if richTextTagIndices[index].name == name:sub(2) then
+
+            -- Add a tag end offset.
+            local _, endOffset = textCopy:find(tagPattern);
+            if endOffset then
+
+              richTextTagIndices[index].endOffset = pointer + endOffset;
+
+            end;
+
+            -- Remove the tag from the open tag table.
+            table.remove(openTagIndices, index);
+            break;
+
+          end
+
+        end
+
+      else
+
+        -- Get the tag start offset.
+        local attributes = firstSpaceIndex and tagText:sub(firstSpaceIndex + 1) or "";
+        table.insert(richTextTagIndices, {
+          name = name;
+          attributes = attributes;
+          startOffset = textCopy:find(tagPattern) :: number + pointer - 1;
+        });
+        table.insert(openTagIndices, #richTextTagIndices);
+
+      end
+
+      -- Remove the tag from our copy.
+      local _, pointerUpdate = textCopy:find(tagPattern);
+      if pointerUpdate then
+
+        pointer += pointerUpdate - 1;
+        textCopy = textCopy:sub(pointerUpdate);
+
+      end;
+
+    end;
+
+  end
+
+  return richTextTagIndices;
+
+end;
+
+function DialogueContentFitter:getLineBreakIndices(textLabel: TextLabel, uiListLayout: UIListLayout): {number}
+
+  -- Iterate through each character.
+  local breakpoints: {number} = {};
+  local lastSpaceIndex: number = 1;
+  local skipCounter = 0;
+  local originalText = textLabel.Text;
+  local remainingRichTextTags = self:getRichTextTags(originalText);
+  textLabel.Text = "";
+
+  for index, character in originalText:split("") do
+
+    -- Check if this is an offset.
+    if skipCounter > 0 then
+
+      skipCounter -= 1;
+      continue;
+
+    end
+
+    if textLabel.RichText then
+
+      for _, richTextTagIndex in remainingRichTextTags do
+
+        if richTextTagIndex.startOffset == index then
+
+          skipCounter = (`<{richTextTagIndex.name}{if richTextTagIndex.attributes and richTextTagIndex.attributes ~= "" then ` {richTextTagIndex.attributes}` else ""}>`):len() - 1;
+          break;
+
+        elseif richTextTagIndex.endOffset :: number - (`</{richTextTagIndex.name}>`):len() == index then
+
+          skipCounter = (`</{richTextTagIndex.name}>`):len() - 1;
+          break;
+
+        end
+
+      end
+
+    end;
+
+    if skipCounter > 0 then
+
+      continue;
+
+    end
+
+    -- Keep track of spaces.
+    if character == " " then
+
+      lastSpaceIndex = index;
+
+    end
+
+    -- Keep track of the original text bounds.
+    local originalAbsoluteContentSize = uiListLayout.AbsoluteContentSize;
+
+    -- Add a character and reformat the text.
+    textLabel.Text = textLabel.ContentText .. character;
+    if textLabel.RichText then
+
+      for _, richTextTagInfo in remainingRichTextTags do
+
+        local tagStartOffset = richTextTagInfo.startOffset;
+        local tagEndOffset = richTextTagInfo.endOffset :: number;
+        if index >= tagStartOffset and tagEndOffset > (breakpoints[#breakpoints] or 0) then
+
+          local prefix = `<{richTextTagInfo.name}{if richTextTagInfo.attributes and richTextTagInfo.attributes ~= "" then ` {richTextTagInfo.attributes}` else ""}>`;
+          local suffix = `</{richTextTagInfo.name}>`;
+          local startOffset = tagStartOffset - (breakpoints[#breakpoints] or 0);
+          local endOffset = (tagEndOffset - (breakpoints[#breakpoints] or 0)) - prefix:len() - suffix:len();
+          textLabel.Text = textLabel.ContentText:sub(1, startOffset - 1) .. prefix .. textLabel.ContentText:sub(startOffset, endOffset - 1) .. suffix .. textLabel.ContentText:sub(endOffset);
+
+        end
+
+      end
+
+    end;
+
+    if uiListLayout.AbsoluteContentSize.Y ~= originalAbsoluteContentSize.Y then
+
+      uiListLayout.Wraps = false;
+      uiListLayout.Wraps = true;
+
+    end
+
+    -- From here, we can guess for a line break because the Y axis of 
+    -- the UIListLayout's content size will change if the character causes a line break.
+    if uiListLayout.AbsoluteContentSize.Y > originalAbsoluteContentSize.Y then
+
+      -- We should check again with unwrapped text to ensure that 
+      -- rich text didn't cause the line break.
+      local wrappedAbsoluteContentSize = uiListLayout.AbsoluteContentSize;
+
+      textLabel.TextWrapped = false;
+
+      if uiListLayout.AbsoluteContentSize.Y < wrappedAbsoluteContentSize.Y then
+
+        table.insert(breakpoints, lastSpaceIndex);
+        textLabel.Text = originalText:sub(lastSpaceIndex + 1, index);
+
+      end
+
+      textLabel.TextWrapped = true;
+
+    end
+
+  end
+
+  textLabel.Text = originalText;
+
+  return breakpoints;
+
+end
+
 function DialogueContentFitter:fitText(text: string, contentContainer: GuiObject, textLabelTemplate: TextLabel, pages: {Page}): (GuiObject, {Page})
 
   pages = self:clonePages(pages);
   local uiListLayout = contentContainer:FindFirstChildOfClass("UIListLayout");
   assert(uiListLayout, "[Dialogue Maker] Content container must have a UIListLayout to fit text.");
 
-  -- Measure the content container size.
-  local contentContainerChildren = contentContainer:GetChildren();
-  local function setContentContainerChildrenParent(parent: Instance?)
-    
-    for _, child in contentContainerChildren do
-
-      if child:IsA("GuiObject") then
-
-        child.Parent = parent;
-
-      end
-
-    end
-
-  end;
-  setContentContainerChildrenParent(nil);
-
-  local frame = Instance.new("Frame");
-  frame.Size = UDim2.fromScale(1, 1);
-  frame.Parent = contentContainer;
-  
-  local canvasSize = frame.AbsoluteSize;
-  local startingAbsolutePosition = frame.AbsolutePosition;
-
-  frame:Destroy();
-
-  setContentContainerChildrenParent(contentContainer);
-  
   -- Fit the text into the content container.
   local canCreateUISizeConstraint = true;
   local remainingText: string = text;
@@ -151,12 +305,13 @@ function DialogueContentFitter:fitText(text: string, contentContainer: GuiObject
     -- Constrain the text label if there is another UI element to the left of it.
     -- This ensures the text doesn't wrap in the middle of the container.
     local uiSizeConstraint: UISizeConstraint?;
-    if canCreateUISizeConstraint and textLabel.AbsolutePosition.X > startingAbsolutePosition.X then
+    if canCreateUISizeConstraint and textLabel.AbsolutePosition.X > contentContainer.AbsolutePosition.X then
 
       textLabel.TextWrapped = false;
 
       local newUISizeConstraint = Instance.new("UISizeConstraint");
-      newUISizeConstraint.MaxSize = Vector2.new(canvasSize.X - (textLabel.AbsolutePosition.X - startingAbsolutePosition.X), math.huge);
+      newUISizeConstraint.Name = "NewLineConstraint";
+      newUISizeConstraint.MaxSize = Vector2.new(contentContainer.AbsoluteSize.X - (textLabel.AbsolutePosition.X - contentContainer.AbsolutePosition.X), math.huge);
       newUISizeConstraint.Parent = textLabel;
       uiSizeConstraint = newUISizeConstraint;
 
@@ -175,7 +330,7 @@ function DialogueContentFitter:fitText(text: string, contentContainer: GuiObject
     textLabel.Text = remainingText;
 
     -- If the text label fits, then we have enough components and pages.
-    if textLabel.TextFits and uiListLayout.AbsoluteContentSize.Y <= canvasSize.Y then
+    if textLabel.TextFits and uiListLayout.AbsoluteContentSize.Y <= contentContainer.AbsoluteSize.Y then
 
       remainingText = "";
 
@@ -196,8 +351,9 @@ function DialogueContentFitter:fitText(text: string, contentContainer: GuiObject
         elseif uiSizeConstraint then
           
           canCreateUISizeConstraint = false;
+          textLabel:Destroy();
 
-        elseif uiListLayout.AbsoluteContentSize.Y > canvasSize.Y then
+        elseif uiListLayout.AbsoluteContentSize.Y > contentContainer.AbsoluteSize.Y then
 
           shouldCreateNewPage = true;
 
@@ -207,12 +363,11 @@ function DialogueContentFitter:fitText(text: string, contentContainer: GuiObject
 
         end;
 
-      until (textLabel.TextFits and uiListLayout.AbsoluteContentSize.Y <= canvasSize.Y) or shouldCreateNewPage or not canCreateUISizeConstraint;
+      until (textLabel.TextFits and uiListLayout.AbsoluteContentSize.Y <= contentContainer.AbsoluteSize.Y) or shouldCreateNewPage or not canCreateUISizeConstraint;
 
       -- If needed, try again without the UISizeConstraint so the text can use a new line.
       if not canCreateUISizeConstraint then
 
-        textLabel:Destroy();
         continue;
 
       end;
@@ -233,215 +388,24 @@ function DialogueContentFitter:fitText(text: string, contentContainer: GuiObject
 
     -- If the text has multiple lines, create another TextLabel to replace the last line of text.
     -- This allows inlined components. Remember how we used the UISizeConstraint to limit the text width?
-    local function getLineBreakPositions(text: string): {number}
-
-      local function getRichTextIndices(text: string): {RichTextTag}
-
-        local richTextTagIndices: {RichTextTag} = {};
-        local openTagIndices: {number} = {};
-        local textCopy = text;
-        local tagPattern = "<[^<>]->";
-        local pointer = 1;
-        for tag in textCopy:gmatch(tagPattern) do
-
-          -- Get the tag name and attributes.
-          local tagText = tag:match("<([^<>]-)>");
-          if tagText then
-
-            local firstSpaceIndex = tagText:find(" ");
-            local tagTextLength = tagText:len();
-            local name = tagText:sub(1, (firstSpaceIndex and firstSpaceIndex - 1) or tagTextLength);
-            if name:sub(1, 1) == "/" then
-
-              for _, index in openTagIndices do
-
-                if richTextTagIndices[index].name == name:sub(2) then
-
-                  -- Add a tag end offset.
-                  local _, endOffset = textCopy:find(tagPattern);
-                  if endOffset then
-
-                    richTextTagIndices[index].endOffset = pointer + endOffset;
-
-                  end;
-
-                  -- Remove the tag from the open tag table.
-                  table.remove(openTagIndices, index);
-                  break;
-
-                end
-
-              end
-
-            else
-
-              -- Get the tag start offset.
-              local attributes = firstSpaceIndex and tagText:sub(firstSpaceIndex + 1) or "";
-              table.insert(richTextTagIndices, {
-                name = name;
-                attributes = attributes;
-                startOffset = textCopy:find(tagPattern) :: number + pointer - 1;
-              });
-              table.insert(openTagIndices, #richTextTagIndices);
-
-            end
-
-            -- Remove the tag from our copy.
-            local _, pointerUpdate = textCopy:find(tagPattern);
-            if pointerUpdate then
-
-              pointer += pointerUpdate - 1;
-              textCopy = textCopy:sub(pointerUpdate);
-
-            end;
-
-          end;
-
-        end
-
-        return richTextTagIndices;
-
-      end
-
-      local restoredComponents = {};
-      for _, component in pages[#pages] do
-
-        if typeof(component) == "string" then
-
-          local TextLabel = textLabel:Clone();
-          TextLabel.Text = component;
-          TextLabel.Parent = contentContainer;
-          table.insert(restoredComponents, TextLabel);
-
-        end
-
-      end;
-
-      -- Iterate through each character.
-      local breakpoints: {number} = {};
-      local lastSpaceIndex: number = 1;
-      local skipCounter = 0;
-      local remainingRichTextTags = getRichTextIndices(text);
-      textLabel.Text = "";
-
-      for index, character in text:split("") do
-
-        -- Check if this is an offset.
-        if skipCounter > 0 then
-
-          skipCounter -= 1;
-          continue;
-
-        end
-
-        if textLabel.RichText then
-
-          for _, richTextTagIndex in remainingRichTextTags do
-
-            if richTextTagIndex.startOffset == index then
-
-              skipCounter = (`<{richTextTagIndex.name}{if richTextTagIndex.attributes and richTextTagIndex.attributes ~= "" then ` {richTextTagIndex.attributes}` else ""}>`):len() - 1;
-              break;
-
-            elseif richTextTagIndex.endOffset :: number - (`</{richTextTagIndex.name}>`):len() == index then
-
-              skipCounter = (`</{richTextTagIndex.name}>`):len() - 1;
-              break;
-
-            end
-
-          end
-
-        end;
-
-        if skipCounter > 0 then
-
-          continue;
-
-        end
-
-        -- Keep track of spaces.
-        if character == " " then
-
-          lastSpaceIndex = index;
-
-        end
-
-        -- Keep track of the original text bounds.
-        local originalAbsoluteContentSize = uiListLayout.AbsoluteContentSize;
-
-        -- Add a character and reformat the text.
-        textLabel.Text = textLabel.ContentText .. character;
-        if textLabel.RichText then
-
-          for _, richTextTagInfo in remainingRichTextTags do
-
-            local tagStartOffset = richTextTagInfo.startOffset;
-            local tagEndOffset = richTextTagInfo.endOffset :: number;
-            if index >= tagStartOffset and tagEndOffset > (breakpoints[#breakpoints] or 0) then
-
-              local prefix = `<{richTextTagInfo.name}{if richTextTagInfo.attributes and richTextTagInfo.attributes ~= "" then ` {richTextTagInfo.attributes}` else ""}>`;
-              local suffix = `</{richTextTagInfo.name}>`;
-              local startOffset = tagStartOffset - (breakpoints[#breakpoints] or 0);
-              local endOffset = (tagEndOffset - (breakpoints[#breakpoints] or 0)) - prefix:len() - suffix:len();
-              textLabel.Text = textLabel.ContentText:sub(1, startOffset - 1) .. prefix .. textLabel.ContentText:sub(startOffset, endOffset - 1) .. suffix .. textLabel.ContentText:sub(endOffset);
-
-            end
-
-          end
-
-        end;
-
-        if uiListLayout.AbsoluteContentSize.Y ~= originalAbsoluteContentSize.Y then
-
-          uiListLayout.Wraps = false;
-          uiListLayout.Wraps = true;
-
-        end
-
-        -- From here, we can guess for a line break because the Y axis of 
-        -- the UIListLayout's content size will change if the character causes a line break.
-        if uiListLayout.AbsoluteContentSize.Y > originalAbsoluteContentSize.Y then
-
-          -- We should check again with unwrapped text to ensure that 
-          -- rich text didn't cause the line break.
-          local wrappedAbsoluteContentSize = uiListLayout.AbsoluteContentSize;
-
-          textLabel.TextWrapped = false;
-
-          if uiListLayout.AbsoluteContentSize.Y < wrappedAbsoluteContentSize.Y then
-
-            table.insert(breakpoints, lastSpaceIndex);
-            textLabel.Text = text:sub(lastSpaceIndex + 1, index);
-
-          end
-
-          textLabel.TextWrapped = true;
-
-        end
-
-      end
-
-      textLabel.Text = text;
-
-      for _, restoredComponent in restoredComponents do
-
-        restoredComponent:Destroy();
-
-      end
-
-      return breakpoints;
-
-    end
-
-    local lineBreaks = getLineBreakPositions(textLabel.Text);
+    local lineBreaks = DialogueContentFitter:getLineBreakIndices(textLabel, uiListLayout);
     local lastLineBreakIndex = lineBreaks[#lineBreaks];
+
+    local function addLineHeight(textLabel: TextLabel)
+
+      local lineHeightConstraint = Instance.new("UISizeConstraint");
+      lineHeightConstraint.Name = "LineHeightConstraint";
+      lineHeightConstraint.MinSize = Vector2.new(0, (#lineBreaks + 1) * textLabel.LineHeight * textLabel.TextSize);
+      lineHeightConstraint.Parent = textLabel;
+
+    end;
 
     if lastLineBreakIndex then
 
       local paragraphTextLabel = textLabel:Clone();
       paragraphTextLabel.Text = textLabel.Text:sub(1, lastLineBreakIndex);
       paragraphTextLabel.Parent = textLabel.Parent;
+      addLineHeight(paragraphTextLabel);
       table.insert(pages[#pages], paragraphTextLabel.Text);
 
       -- Put the remaining text in the original TextLabel.
@@ -449,6 +413,7 @@ function DialogueContentFitter:fitText(text: string, contentContainer: GuiObject
 
     end;
 
+    addLineHeight(textLabel);
     table.insert(pages[#pages], textLabel.Text);
 
   until remainingText == "";
